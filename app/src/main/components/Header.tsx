077c80c988f51299d9d1db73f4dbc5d1c7a5fb25
koji.inoue <koji.inoue@sansan.com>
2021-09-20 16:09:25 +0900

    アイコン作成とogp設定

--- app/src/main/components/Header.tsx
+++ app/src/main/components/Header.tsx
@@ -1,12 +1,17 @@
 
-import { Flex, Heading, Spacer } from '@chakra-ui/react'
+import { Image, Flex, Heading, Spacer } from '@chakra-ui/react'
 import React, { FC } from 'react'
 import { ColorModeSwitcher } from '../../shared/components/ColorModeSwitcher'
 
 export const Header: FC = () => {
   return (
     <Flex h='100%' justify='center' align='center'>
-      <Heading as="h1" size="md" isTruncated>
+      <Image
+        borderRadius="full"
+        boxSize="32px"
+        src={`${process.env.PUBLIC_URL}/favicon.ico`}
+      />
+      <Heading as="h1" size="md" marginLeft="4" isTruncated>
         PEライン糸巻量計算ツール
       </Heading>
       <Spacer />
